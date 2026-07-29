1176041d45d6d417d160c229376090e60b601272
Chizter Ivan L. Tejada <202580389@psu.palawan.edu.ph>
2026-07-29 22:00:06 +0800

    Create problem2.cs

---
+++ problem2.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Chizter";
+        string lastName = "Tejada";
+
+        string fullName = firstName + " " + lastName;
+
+        Console.WriteLine("Full Name: " + fullName);
+    }
+}
